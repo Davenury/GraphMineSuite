5ba680f2084e0893ce4510d5b776aea4c8a2a402
Rados13 <radoslawszuma@gmail.com>
2022-06-15 18:48:29 +0200

    Improve load_dataset.sh script

--- scripts/load_dataset.sh
+++ scripts/load_dataset.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+directory=$(dirname $0)
+
 wget http://snap.stanford.edu/data/twitter.tar.gz
-tar -xvf twitter.tar.gz
-mv ./twitter ../twitter
+tar -xvf twitter.tar.gz -C $directory/../
