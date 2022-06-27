029f3ba0959d8d4d3230b4338c7333d63ef9703b
Radosław Szuma <56085570+Rados13@users.noreply.github.com>
2022-06-27 14:41:11 +0200

    Fix awk

--- scripts/run_aws.sh
+++ scripts/run_aws.sh
@@ -17,8 +17,7 @@ aws s3 cp $directory/bootstrap.sh s3://$bucket/
 rm -f ./GMS.zip
 
 echo $bucket
-cluster_id=$(aws emr create-cluster --applications Name=Spark Name=Zeppelin --name 'GMS cluster' --ec2-attributes '{"KeyName":"vockey"}' --use-default-roles --enable-debugging --log-uri "s3n://$bucket/elasticmapreduce/" --release-label emr-5.35.0 --instance-type m5.xlarge --instance-count 3 --bootstrap-actions '[{"Path":"'"s3://$bucket/bootstrap.sh"'","Args":["'$bucket'"],"Name":"Bootstrap action"}]' | grep "ClusterId" | awk --field-separator=":" '{print substr($2,3,length($2)-4)}')
+cluster_id=$(aws emr create-cluster --applications Name=Spark Name=Zeppelin --name 'GMS cluster' --ec2-attributes '{"KeyName":"vockey"}' --use-default-roles --enable-debugging --log-uri "s3n://$bucket/elasticmapreduce/" --release-label emr-5.35.0 --instance-type m5.xlarge --instance-count 3 --bootstrap-actions '[{"Path":"'"s3://$bucket/bootstrap.sh"'","Args":["'$bucket'"],"Name":"Bootstrap action"}]' | grep "ClusterId" | awk -F: '{print substr($2,3,length($2)-4)}')
 
-cluster_id="j-2ZXWXV26N8OPY"
 echo $cluster_id
 aws emr add-steps --cluster-id $cluster_id --steps Type=spark,Name=GraphProcessing,Args=[--master,yarn,--py-files,s3://$bucket/GMS.zip,--conf,spark.yarn.submit.waitAppCompletion=true,--num-executors,3,--executor-cores,3,--packages,graphframes:graphframes:0.6.0-spark2.3-s_2.11,--executor-memory,10g,s3://$bucket/GMS/aws_run.py,"s3://$bucket/$dataset","$bucket"]
